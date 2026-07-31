085335a647893c6334f33a4bec1be2f0c5c83029
Gabriel Sanabria <gabriel.sanabriacs@uanl.edu.mx>
2026-07-30 21:15:45 -0600

    Visual and control differences between player 1 and player 2

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,39 +1,61 @@
 extends CharacterBody3D
 
-# Velocidad de player, y acelearacion de caida.
+# Enumeracion de IDs posibles de jugadores.
+enum PlayerId { PLAYER_1, PLAYER_2 }
+
+# Propiedades publicas del script.
+@export var player_id : PlayerId
+@export var mat_player_1 : Material
+@export var mat_player_2 : Material
 @export var speed = 14
 @export var fall_acceleration = 48
-@export var jump_impulse = 20
+@export var jump_impulse = 25
 
-# Propiedad necesaria para gravedad.
+# Propiedades privadas del script.
 var target_velocity = Vector3.ZERO
 
-# Funcion de procesamiento de fisicas. Tambien teine metido el movimiento.
+func _ready() -> void:
+	# Cambiar el color del jugador dependiendo su ID.
+	match player_id:
+		PlayerId.PLAYER_1:
+			$Pivot/Character.material_override = mat_player_1
+		PlayerId.PLAYER_2:
+			$Pivot/Character.material_override = mat_player_2
+
+# Funcion de procesamiento de fisicas. Tambien tiene metido el movimiento.
 func _physics_process(delta):
 	var direction = Vector3.ZERO
+	var is_jumping = false
 	
-	# Direcciones del player
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
+	# Direcciones de movimiento y salto según el Id de jugador.
+	match player_id:
+		PlayerId.PLAYER_1:
+			if Input.is_key_pressed(KEY_D): # Derecha J1
+				direction.x += 1
+			if Input.is_key_pressed(KEY_A): # Izquierda J1
+				direction.x -= 1
+			is_jumping = Input.is_key_pressed(KEY_W) # Salto J1
+		PlayerId.PLAYER_2:
+			if Input.is_key_pressed(KEY_RIGHT): # Derecha J2
+				direction.x += 1
+			if Input.is_key_pressed(KEY_LEFT): # Izquierda J2
+				direction.x -= 1
+			is_jumping = Input.is_key_pressed(KEY_UP) # Salto J2
 	
-	# Mover direccion visual plaeyer
+	# Mover direccion visual plaeyer.
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.basis = Basis.looking_at(direction)
 	
-	# Volocidad en terra
+	# Volocidad en terra.
 	target_velocity.x = direction.x * speed
 		
-	# Volocidad vertical. Gravedad
-	var jump = (Input.is_action_pressed("jump") == true ) or (Input.is_action_pressed("move_up") == true)
+	# Aplicar gravedad o impulso de salto segun el caso.
 	if not is_on_floor():
 		target_velocity.y -= fall_acceleration * delta
-	else:
-		if jump:
-			target_velocity.y = jump_impulse
-	
-	# Mover personaje
+	elif is_jumping:
+		target_velocity.y = jump_impulse
+		
+	# Aplicar movimiento del personaje.
 	velocity = target_velocity
 	move_and_slide()
