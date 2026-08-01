5614035542f370638f53bc60f81ea48e401d0f12
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-07-31 23:00:48 -0600

    animations

--- scripts/character.gd
+++ scripts/character.gd
@@ -10,8 +10,10 @@ const MAX_JUMPS := 2
 @export var jump_impulse := 25
 @export var init_looking_at_right : bool = false
 
-# Propiedades publicas | Materiales
+
+# Propiedades publicas | Materiales y malla
 @export var material : Material = null
+@export var mesh : Mesh = null
 
 # Propiedades privadas | Movimientos
 var move_left: bool = false
@@ -24,6 +26,7 @@ var jump: bool = false
 var target_velocity = Vector3.ZERO
 var jump_count = 0
 var was_jumping = false
+var air_count = 0
 
 # Funciones
 func _get_initial_facing() -> Vector3:
@@ -32,21 +35,29 @@ func _get_initial_facing() -> Vector3:
 	else:
 		return Vector3.LEFT
 
-func _set_material() -> void:
+func _set_material( m: Material ) -> void:
 	'''
 	Establecer material al jugador.
 	Aunque preferiblemente sea nomas color. 
 	Pero por ahroa, asi ta bien.
 	'''
 	if material != null:
-		$Pivot/CharacterMesh.material_override = material
+		$Pivot/Mesh.material_override = m
+
+func _set_mesh( m: Mesh ) -> void:
+	'''
+	Establecer malla nueva
+	'''
+	if m != null:
+		$Pivot/Mesh.mesh = m
 
 func _ready() -> void:
 	'''
 	Inicializar el character, con sus colorines, materiales etc.
 	'''
 	# Cambiar el color del character
-	_set_material()
+	_set_mesh(mesh)
+	_set_material(material)
 	
 	# Establecer direccion de vista inicial.
 	$Pivot.basis = Basis.looking_at( _get_initial_facing() )
@@ -71,29 +82,58 @@ func _physics_process(delta: float) -> void:
 	_collect_input()
 	
 	var direction := _get_move_direction()
+	var on_floor := is_on_floor()
 		
 	# Calcular velocidad en tierra. Determinar salto
 	target_velocity.x = direction.x * speed
 	var is_jumping := jump or move_up
 	
-	# Detectar si la tecla de salto se acaba de presionar.
+	# Gravedad | Fuerza vertical. 
+	# Contadores a cero, para que no se acumule fuerza vertcal.
+	if on_floor:
+		target_velocity.y = 0
+		air_count = 0
+		jump_count = 0
+	else:
+		target_velocity.y -= fall_acceleration * delta
+		air_count += 1
+	
+	# Salto | Detectar si la tecla de salto se acaba de presionar.
 	var jump_pressed := is_jumping and not was_jumping
 	was_jumping = is_jumping
 
-	# Aplicar salto/doble salto o gravedad segun el caso.
+	# Salto | Aplicar salto normal o doble segun el caso.
 	if jump_pressed and jump_count < MAX_JUMPS:
 		target_velocity.y = jump_impulse
-		jump_count += 1
-	elif not is_on_floor():
-		target_velocity.y -= fall_acceleration * delta
+		if air_count > 0:
+			jump_count = MAX_JUMPS
+		else:
+			jump_count += 1
+	
+	# Characeter action states
+	var moving = direction.x != 0.0
+	var jumping = target_velocity.y > 0
+	var falling = not jumping and not on_floor
+	var neutral = on_floor and not moving
+	var running = on_floor and moving
 		
-	# Reiniciar el contador de saltos al aterrizar.
-	if is_on_floor() and not jump_pressed:
-		jump_count = 0
+	# Animacion | Movimiento en el piso
+	if neutral:
+		$Pivot/Mesh.rotation.x = 0
+	if running:
+		if not $AnimationMesh.is_playing():
+			$AnimationMesh.play("run")
+	else:
+		$AnimationMesh.stop()
+	# Animacion | Salto
+	if jumping:
+		$Pivot/Mesh.rotation.x = -50
+	elif falling:
+		$Pivot/Mesh.rotation.x = 50
 		
 	# Animación | Mover direccion visual del player.
-	if direction.x != 0.0:
-		$Pivot.basis = Basis.looking_at(Vector3(direction.x, 0.0, 0.0))
+	if moving:
+		$Pivot.basis = Basis.looking_at(direction)
 
 	# Aplicar movimiento del personaje.
 	velocity = target_velocity
