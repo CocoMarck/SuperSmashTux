e39206f791022ec8e17c4f706cf52fe8a01fbd7e
Jean Abraham Chacón Candanosa <jeanabrahamchc@gmail.com>
2026-09-21 17:32:09 -0600

    Raneme menu scenes

--- scripts/menu/button_controls.gd
+++ scripts/menu/button_controls.gd
@@ -1,13 +1,13 @@
 extends Button
 
-const CONTROL_SCENE := "res://scenes/controls.tscn"
+const CONTROL_MENU_SCENE := "res://scenes/controls_menu.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pressed.connect(_open_controls)
+	pressed.connect(_open_controls_menu)
 
-func _open_controls() -> void:
-	get_tree().change_scene_to_file(CONTROL_SCENE)
+func _open_controls_menu() -> void:
+	get_tree().change_scene_to_file(CONTROL_MENU_SCENE)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- scripts/menu/button_menu.gd
+++ scripts/menu/button_menu.gd
@@ -1,13 +1,13 @@
 extends Button
 
-const MENU_SCENE := "res://scenes/menu.tscn"
+const MAION_MENU_SCENE := "res://scenes/main_menu.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pressed.connect(_open_menu)
+	pressed.connect(_open_main_menu)
 
-func _open_menu() -> void:
-	get_tree().change_scene_to_file(MENU_SCENE)
+func _open_main_menu() -> void:
+	get_tree().change_scene_to_file(MAION_MENU_SCENE)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
